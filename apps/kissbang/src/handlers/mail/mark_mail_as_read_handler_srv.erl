7914f2a9b0f2ca7d5a18ac9013bb92064ecdcf42
dehun <retnuhed@gmail.com>
2012-10-10 17:00:28 +0000

    [mail] fixed mark mail as read

--- apps/kissbang/src/handlers/mail/mark_mail_as_read_handler_srv.erl
+++ apps/kissbang/src/handlers/mail/mark_mail_as_read_handler_srv.erl
@@ -130,6 +130,6 @@ code_change(_OldVsn, State, _Extra) ->
 %%% Internal functions
 %%%===================================================================
 handle_mark_mail_as_read(CallerGuid, MailGuid) ->
-    ok = mail_srv:mark_mail_as_read(CallerGuid, MailGuid),
+    ok = mail_srv:mark_mail_as_read(MailGuid),
     proxy_srv:async_route_messages(CallerGuid, [#on_mail_marked_as_read{marked_mail_guid = MailGuid}]).
     
