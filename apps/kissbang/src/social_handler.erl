b3d73f5ad1e51ead35cdb252cb66163a8f609740
y.netesov <y.netesov@localhost>
2012-10-22 14:52:31 +0000

    [socialcallback] keep implementing social callback.

--- apps/kissbang/src/social_handler.erl
+++ apps/kissbang/src/social_handler.erl
@@ -2,5 +2,5 @@
 
 -export([handle_social_data/2]).
 
-handle_social_data(HandlerPid, Data) ->
-    gen_server:cast(HandlerPid, {handle_social_callback, Data}).
+handle_social_data(HandlerPid, Req) ->
+    gen_server:cast(HandlerPid, {handle_social_callback, Req}).
