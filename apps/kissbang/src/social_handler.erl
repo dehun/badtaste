992d8604eee51dec227960207169497d44b07c35
y.netesov <y.netesov@localhost>
2012-10-25 11:48:45 +0000

    [social_handler] fix of on_item_bought

--- apps/kissbang/src/social_handler.erl
+++ apps/kissbang/src/social_handler.erl
@@ -1,11 +1,13 @@
 -module(social_handler).
 
+-include("social_handlers/item.hrl").
 -export([handle_social_data/2]).
 
 handle_social_data(HandlerPid, Req) ->
     gen_server:cast(HandlerPid, {handle_social_callback, Req}).
 
 on_item_bought(UserId, Item) ->
+    ItemId = Item#item.item_id,
     {true, Guid} = auth_srv:is_registered(UserId),
     case Item#item.type of
         "gold" ->
